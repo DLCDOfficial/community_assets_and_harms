511527883e27050db7757112fc4007a5950b671e
Hgedlaman <hannah.gedlaman@dlcd.oregon.gov>
2025-10-13 16:33:41 -0700

    refactor: remove unneccesary functions, clean up docs

--- src/calculate.js
+++ src/calculate.js
@@ -1,17 +1,26 @@
-// this file contains functions to calculate quartiles and assign bins
-// as well as the main calculation function that returns a string to inform the rendering of the map
+// This file contains the main calculation function to render colored hexes.
+// & helper functions to facilitate this
+
+//Hexes are colored based on average harms and assets values
+// These scores are calculated based on the selected indicators and the comparison region (state, county, ugb)
+// Each hex is assigned a bin (1-4) for harms and assets based on the average value for that hex
+// The bin numbers are then combined into a string like "1,3" which maps to a defined color
 import { formatHeader } from './htmlHelpers.js';
+
+
 
 /**
  * assign a bin (1-4) for a value based on provided thresholds
- * 
- * This function is called to assign a bin number for assets and harms, which is then used to create a string like "1,3" that informs the rendering of the map.
+ * This function is called to assign a bin number for assets and harms
  * 
  * @param {double} value the numeric value to bin
- * @param {{q1: number, q2: number, q3: number}} an object with q1, q2, and q3 properties representing the quartile thresholds. this is returned from getQuartileThresholds.
- * @returns {int} the bin number (1-4). the bin number is used to facilitate rendering the map with different colors for each bin.
+ * @returns {int} the bin number (1-4). the bin number is used to facilitate rendering the map with different colors.
  */
-function assignBin(value, thresholds) {
+function assignBin(value) {
+
+  // the quartile thresholds for the bins
+  const thresholds = {q1:.25,q2:.5,q3:.75};
+
   if (value <= thresholds.q1) return 1;
   if (value <= thresholds.q2) return 2;
   if (value <= thresholds.q3) return 3;
@@ -21,56 +30,51 @@ function assignBin(value, thresholds) {
 /**
  * 
  * @param {int} bin // the bin number (1-4)
- * @param {*} percentage_strings // an object mapping bin numbers to percentile strings, e.g. {1: "0-25%", 2: "25-50%", 3: "50-75%", 4: "75-100%"}
  * @returns  {string|null} the corresponding percentile string, or null if bin is out of range
  */
-function getPercentileFromBin(bin, percentage_strings) {
-  switch (bin) {
-    case 1:
-      return percentage_strings[1];
-    case 2:
-      return percentage_strings[2];
-    case 3:
-      return percentage_strings[3];
-    case 4:
-      return percentage_strings[4]; // max percentile
-    default:
-      return null;
-  }}
+function getPercentileFromBin(bin) {
+  //mapping of bin numbers to percentile strings
+  const percentage_strings = { 1: "0-25%", 2: "25-50%", 3: "50-75%", 4: "75-100%" };
+  return percentage_strings[bin] || null;}
 
 
 //standard thresholds. We could change these if we wanted. 
 
-const percentage_strings = { 1: "0-25%", 2: "25-50%", 3: "50-75%", 4: "75-100%" };
 
-
-const fixed_thresholds = {q1:.25,q2:.5,q3:.75};
-
-
+/**
+ * This function is a helper to build the display string for the popup.
+ * It appends the newString to the appropriate bin in the displayStringObject.
+ * This ensures that when we build the final display string, the variables are displayed from highest to lowest percentile.
+ * 
+ * @param {int} bin // the bin number (1-4)
+ * @param {obj} displayStringObject // the object holding strings for each bin. 
+ * @param {string} newString // the string to append (a single variable and its value)
+ * @returns 
+ */
 function appendDisplayString(bin, displayStringObject, newString) {
-  switch (bin) {
-    case 1:
-      displayStringObject[1] += newString
-      break;
-    case 2:
-      displayStringObject[2] += newString
-      break;
-    case 3:
-      displayStringObject[3] += newString
-      break;
-    case 4:
-      displayStringObject[4] += newString
-      break;
-    default:
-      break;} }
-
-
+  if(bin == 1){
+    displayStringObject[1] += newString
+    return;
+  }
+  if(bin == 2){
+    displayStringObject[2] += newString
+    return;
+  }
+  if(bin == 3){
+    displayStringObject[3] += newString
+    return;
+  }
+  if(bin == 4){
+    displayStringObject[4] += newString
+    return;
+  }
+  return;}
 /**
  * main calculation function.
  * 
  * Calculates average values for harms and assets, then assigns bins based on thresholds.
  * @param {string} field the percentage rank field to use for calculations. ugb, county or state.
- * @param {object} rows <p>the data rows for a single hexagon. A dictionary where the key is the hex ID, 
+ * @param {object} rows the data rows for a single hexagon. A dictionary where the key is the hex ID, 
  * and the value is an array of objects where each row is a variable (harm or asset) with its value and type.</p>
  * 
  * @returns {{avg_harms: number, avg_assets: number, quartile_string: string}} avg_harms, avg_assets, and a quartile_string like "1,3" that informs the rendering of the map.
@@ -83,6 +87,8 @@ const calculateValue = (field = 'ugb_pct_rank', rows = [], indicator_set) => {
   //number of harms/assets counted (denominator for averaging)
   let countHarms = 0;
   let countAssets = 0;
+
+  //object to hold strings for each bin to facilitate ordered display in popup
   let displayStringObject = {1: '', 2: '', 3: '', 4: ''};
 
   //string to display in popup
@@ -94,9 +100,9 @@ const calculateValue = (field = 'ugb_pct_rank', rows = [], indicator_set) => {
     if (!indicator_set.has(row.var)) return;
 
     const value = row[field];
-    const quartileValue = assignBin(value, fixed_thresholds);
+    const quartileValue = assignBin(value);
 
-    const percentageRange = getPercentileFromBin(quartileValue, percentage_strings);
+    const percentageRange = getPercentileFromBin(quartileValue);
       
     const formatted_var = formatHeader(row.var)
     let currentString = `${formatted_var}: ${percentageRange} <br>`;
@@ -133,15 +139,15 @@ const calculateValue = (field = 'ugb_pct_rank', rows = [], indicator_set) => {
   const avgHarms = countHarms > 0 ? totalHarms / countHarms : 0;
   const avgAssets = countAssets > 0 ? totalAssets / countAssets : 0;
 
-  const harmsBin = assignBin(avgHarms, fixed_thresholds);
-  const assetsBin = assignBin(avgAssets, fixed_thresholds);
+  const harmsBin = assignBin(avgHarms);
+  const assetsBin = assignBin(avgAssets);
 
-  const harmsRange = getPercentileFromBin(harmsBin, percentage_strings);
-  const assetsRange = getPercentileFromBin(assetsBin, percentage_strings);
+  const harmsRange = getPercentileFromBin(harmsBin);
+  const assetsRange = getPercentileFromBin(assetsBin);
 
   // create the quartile string for rendering
   // based on what percentile bin the average harms and assets fall into
-  const quartile_string = `${assignBin(avgAssets, fixed_thresholds)},${assignBin(avgHarms, fixed_thresholds)}`;
+  const quartile_string = `${assignBin(avgAssets)},${assignBin(avgHarms)}`;
 
   return {
     avg_harms: harmsRange,
